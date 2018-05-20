7a7b2e37f84f59b266dba3505013e7be5ca38d13
Luke Parker <lukep@digispend.io>
2018-05-20 10:31:14 -0500

    Updated main.nim

--- main.nim
+++ main.nim
@@ -1,8 +1,34 @@
-import lib/time
+import lib/UInt
 
 import Reputation/Block
 import Reputation/Blockchain
 
+var a, b: UInt
+a = newUInt("8")
+b = newUInt("6")
+echo "2: " & $(a - b)
+
+a = newUInt("10")
+b = newUInt("8")
+echo "2: " & $(a - b)
+
+a = newUInt("1000")
+b = newUInt("998")
+echo "2: " & $(a - b)
+
+a = newUInt("1001")
+b = newUInt("999")
+echo "2: " & $(a - b)
+
+a = newUInt("1111")
+b = newUInt("999")
+echo "2: " & $(a - b)
+
+a = newUInt("1111")
+b = newUInt("1111")
+echo "0: " & $(a - b)
+
+discard """
 var
     blockchain: Blockchain = createBlockchain("0")
     newBlock: Block
@@ -31,3 +57,4 @@ inc(nonce)
 
 newBlock = createBlock(6, getTime() + 310, "1", "d")
 blockchain.addBlock(newBlock)
+"""
