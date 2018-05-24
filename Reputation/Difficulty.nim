ba6eb582b45887fac8c9bec78c40c87ad0707851
Luke Parker <lukep@digispend.io>
2018-05-23 21:33:11 -0500

    Port to BN; fix a wrong substr

--- Reputation/Difficulty.nim
+++ Reputation/Difficulty.nim
@@ -1,5 +1,5 @@
 import ../lib/Hex
-import ../lib/UInt
+import ../lib/BN
 
 import ./Block as BlockFile
 
@@ -8,15 +8,15 @@ import lists, strutils
 import os
 
 type Difficulty* = ref object of RootObj
-    start*: UInt
-    endTime*: UInt
+    start*: BN
+    endTime*: BN
     difficulty*: string
 
 proc verifyDifficulty*(diff: Difficulty, newBlock: Block) =
     if diff.endTime < newBlock.time:
         raise newException(Exception, "Wrong Difficulty")
 
-    if Hex.revert(diff.difficulty) > Hex.revert(newBlock.hash.substr(0, 7)):
+    if Hex.revert(diff.difficulty) > Hex.revert(newBlock.hash.substr(96, 103)):
         echo "Hash is too low:  " & $Hex.revert(newBlock.hash.substr(96, 103))
         echo "Must be at least: " & $Hex.revert(diff.difficulty)
         raise newException(Exception, "The hash is too low")
@@ -26,8 +26,8 @@ proc calculateNextDifficulty*(blocks: DoublyLinkedList[Block], difficulties: Dou
     echo "Readjusting difficulty"
     sleep(3000)
     var
-        start: UInt = difficulties.tail.value.start
-        endTime: UInt = difficulties.tail.value.endTime
+        start: BN = difficulties.tail.value.start
+        endTime: BN = difficulties.tail.value.endTime
         lastDifficulty: string = difficulties.tail.value.difficulty
         blockCount: uint32 = (uint32) 0
         rate: float32
@@ -43,13 +43,13 @@ proc calculateNextDifficulty*(blocks: DoublyLinkedList[Block], difficulties: Dou
     rate = rate / ((float32) 5) #Target block time in seconds
     echo "New rate: " & $rate
     rate = (((float32) 1) / rate) * (float32) 1000
-    strRate = $(Hex.revert(lastDifficulty) * newUInt(($rate).split(".")[0]))
+    strRate = $(Hex.revert(lastDifficulty) * newBN(($rate).split(".")[0]))
     strRate.insert(".", strRate.len-3)
     echo "strRate:  & strRate"
     rate = parseFloat(strRate)
 
     result = Difficulty(
         start: endTime,
-        endTime: endTime + newUInt("60"),
-        difficulty: Hex.convert(newUInt(($rate).split(".")[0]))
+        endTime: endTime + newBN("60"),
+        difficulty: Hex.convert(newBN(($rate).split(".")[0]))
     )
