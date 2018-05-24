c19561ae61524a5982e5607bb5b851860e9c9144
Luke Parker <lukep@digispend.io>
2018-05-23 21:32:00 -0500

    Move SHA512 lib around

--- lib/SHA512.nim
+++ lib/SHA512.nim
@@ -1,3 +1,3 @@
-proc cSHA512(hexData: cstring): cstring {.header: "../lib/c/SHA512.h", importc: "sha512".}
+proc cSHA512(hexData: cstring): cstring {.header: "../lib/SHA512/SHA512.h", importc: "sha512".}
 proc SHA512*(hex: string): string =
     result = $cSHA512(hex)
