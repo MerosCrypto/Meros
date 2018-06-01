71d457db60d30692e25a2c6dc7f6ea40363fad1f
Luke Parker <lukep@digispend.io>
2018-05-31 21:13:04 -0500

    Add PrivKey/PubKey (pushed in the last commit)

--- src/Wallet/PrivateKey.nim
+++ src/Wallet/PrivateKey.nim
@@ -1,2 +1,24 @@
+import ../lib/RNG
+import ../lib/SECP256K1
+
+import strutils
+
 type PrivKey* = ref object of RootObj
     secret: array[32, uint8]
+
+proc newPrivKey*(): PrivKey =
+    result = PrivKey()
+    result.secret[0..31] = random(32)[0..31]
+
+proc newPrivKey*(hex: string): PrivKey =
+    result = PrivKey()
+    for i in countup(0, 63, 2):
+        result.secret[(int) i/2] = (uint8) parseHexInt($hex[i] & $hex[i+1])
+
+proc `$`*(key: PrivKey): string =
+    result = ""
+    for i in 0 ..< 32:
+        result = result & key.secret[i].toHex()
+
+proc sign*(key: PrivKey, hex: string): string =
+    result = ""
