cc85396affc420ad42a66333ca264d15db852570
Luke Parker <lukep@digispend.io>
2018-07-12 14:30:00 -0500

    Add comments to Merit.

--- src/Merit/Merit.nim
+++ src/Merit/Merit.nim
@@ -1,37 +1,53 @@
+#Number lib.
 import ../lib/BN
 
+#Block/Blockchain/State libs.
 import Block as BlockFile
 import Blockchain
 import State
-
-import lists
-
+#Export the Block lib for miners.
 export BlockFile
 
+#Lists standard lib.
+import lists
+
+#Merit master object for a blockchain and state.
 type Merit* = ref object of RootObj
     blockchain: Blockchain
     state: State
 
+#Creates A Merit object based on a Gensis string.
 proc newMerit*(genesis: string): Merit {.raises: [ValueError, OverflowError, AssertionError, Exception].} =
     result = Merit(
         blockchain: newBlockchain(genesis),
         state: newState()
     )
 
+#Tests a block.
 proc testBlock*(merit: Merit, newBlock: Block): bool {.raises: [OverflowError, AssertionError, Exception].} =
     result = true
+    #If the blockchain rejects the block...
     if not merit.blockchain.testBlock(newBlock):
         result = false
         return
 
+#Add a block.
 proc processBlock*(merit: Merit, newBlock: Block): bool {.raises: [OverflowError, AssertionError, Exception].} =
     result = true
+    #Add the block to the Blockchain.
     if not merit.blockchain.addBlock(newBlock):
+        #If that fails, return false.
         result = false
         return
 
+    #Have the state process the block.
     merit.state.processBlock(newBlock)
 
+#Getters for:
+#The genesis string.
+#The blockchain height.
+#Blocks (and an iterator for them).
+#An address's balance.
 proc getGenesis*(merit: Merit): string {.raises: [].} =
     result = merit.blockchain.getGenesis()
 
--- src/lib/util.nim
+++ src/lib/util.nim
@@ -1,7 +1,7 @@
 #Exponentiation of a function.
 #f^n(x) = f(f(f(... n times ... f(f(f(x))))))
-proc `^`*[T](f: proc(x: T): T, power: int): proc(x: T): T =
-    return proc(x: T): T =
+proc `^`*[T](f: proc(x: T): T, power: int): proc(x: T): T {.raises: [Exception].} =
+    return proc(x: T): T {.raises: [Exception].} =
         result = x
         for _ in 1 .. power:
             result = f(result)
