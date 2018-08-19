f2ecb013a3a262eaa4325414b43bd9d5dbd38a79
Luke Parker <lukep@digispend.io>
2018-08-18 20:15:02 -0500

    Fix Parse Receive.

--- src/Network/Serialize/ParseReceive.nim
+++ src/Network/Serialize/ParseReceive.nim
@@ -1,6 +1,9 @@
 #Errors lib.
 import ../../lib/Errors
 
+#Util lib.
+import ../../lib/Util
+
 #Numerical libs.
 import ../../lib/BN
 import ../../lib/Base
@@ -10,7 +13,7 @@ import ../../Wallet/Address
 import ../../Wallet/Wallet
 
 #SHA512 lib.
-import ../../lib/Argon
+import ../../lib/SHA512
 
 #Node object and Receive object.
 import ../../Database/Lattice/objects/NodeObj
@@ -29,17 +32,17 @@ proc parse*(recvStr: string): Receive {.raises: [ValueError, Exception].} =
         #Public Key | Nonce | Input Address | Input Nonce | Amount | Signature
         recvSeq: seq[string] = recvStr.split(delim)
         #Get the sender's Public Key.
-        sender: PublicKey = recvSeq[0].toBN(255).toString(16).newPublicKey()
+        sender: PublicKey = recvSeq[0].toBN(255).toString(16).pad(64).newPublicKey()
         #Get the nonce.
         nonce: BN = recvSeq[1].toBN(255)
         #Get the input Address.
-        inputAddress: string = recvSeq[2].toBN(255).toString(16).newAddress()
+        inputAddress: string = recvSeq[2].toBN(255).toString(16).pad(64).newAddress()
         #Get the input nonce.
         inputNonce: BN = recvSeq[3].toBN(255)
         #Get the amount.
         amount: BN = recvSeq[4].toBN(255)
         #Get the signature.
-        signature: string = recvSeq[5].toBN(255).toString(16)
+        signature: string = recvSeq[5].toBN(255).toString(16).pad(128)
 
     #Create the Receive.
     result = newReceiveObj(
@@ -55,6 +58,10 @@ proc parse*(recvStr: string): Receive {.raises: [ValueError, Exception].} =
     #Set the nonce.
     if not result.setNonce(nonce):
         raise newException(ValueError, "Couldn't set the Node's nonce.")
+
+    #Set the hash.
+    if result.setHash(SHA512(result.serialize())) == false:
+        raise newException(ResultError, "Couldn't set the node hash.")
 
     #Verify the signature.
     if sender.verify(result.getHash(), signature) == false:
