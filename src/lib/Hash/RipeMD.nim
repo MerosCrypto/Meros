c249205d2f72e4514014bf6e70ede0782ca19cc9
Luke Parker <lukep@digispend.io>
2018-09-05 18:25:18 -0500

    Support empty string hashing.

--- src/lib/Hash/RipeMD.nim
+++ src/lib/Hash/RipeMD.nim
@@ -1,10 +1,17 @@
 #nimcrypto lib.
 import nimcrypto
 
+#Empty ptr uint8.
+var empty: ptr uint8
+
 #RIPEMD 160 hash function.
 proc RipeMD_160*(bytesArg: string): string =
     #Copy the bytes argument.
     var bytes: string = bytesArg
+
+    #If it's an empty string...
+    if bytes.len == 0:
+        return $ripemd160.digest(empty, uint(bytes.len))
 
     #Digest the byte array.
     result = $ripemd160.digest(cast[ptr uint8](addr bytes[0]), uint(bytes.len))
--- src/lib/Hash/SHA2.nim
+++ src/lib/Hash/SHA2.nim
@@ -1,10 +1,17 @@
 #nimcrypto lib.
 import nimcrypto
 
+#Empty ptr uint8.
+var empty: ptr uint8
+
 #SHA2 256 hash function.
 proc SHA2_256*(bytesArg: string): string =
     #Copy the bytes argument.
     var bytes: string = bytesArg
+
+    #If it's an empty string...
+    if bytes.len == 0:
+        return $sha256.digest(empty, uint(bytes.len))
 
     #Digest the byte array.
     result = $sha256.digest(cast[ptr uint8](addr bytes[0]), uint(bytes.len))
@@ -13,6 +20,10 @@ proc SHA2_256*(bytesArg: string): string =
 proc SHA2_512*(bytesArg: string): string =
     #Copy the bytes argument.
     var bytes: string = bytesArg
+
+    #If it's an empty string...
+    if bytes.len == 0:
+        return $sha512.digest(empty, uint(bytes.len))
 
     #Digest the byte array.
     result = $sha512.digest(cast[ptr uint8](addr bytes[0]), uint(bytes.len))
