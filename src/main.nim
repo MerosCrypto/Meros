dcabdcd56ca221551eaeb0384845abc9fbf286f6
Luke Parker <lukep@digispend.io>
2018-09-08 18:03:44 -0500

    Update main with the new Networking lib.

--- src/main.nim
+++ src/main.nim
@@ -10,8 +10,13 @@ import Database/Lattice/Lattice
 #Network lib.
 import Network/Network
 
+#Event lib.
+import ec_events
+
 #Async standard lib.
 import asyncdispatch
+
+#---------- Lattice ----------
 
 var
     minter: Wallet = newWallet()     #Wallet.
@@ -24,8 +29,6 @@ var
         mintIndex,
         newBN()
     )
-    network: Network = newNetwork(0) #Netowk object.
-
 #Sign and add the Mint Receive.
 discard minter.sign(mintRecv)
 discard lattice.add(mintRecv)
@@ -33,7 +36,53 @@ discard lattice.add(mintRecv)
 #Print the Private Key and address of the address holding the coins.
 echo minter.getAddress() &
     " was minted, and has received, one million coins. Its Private Key is " &
-    $minter.getPrivateKey() &
-    "."
+    $minter.getPrivateKey() & "."
+
+#---------- Network ----------
+var
+    events: EventEmitter = newEventEmitter() #EventEmitter for the Network.
+    network: Network = newNetwork(0, events) #Network object.
+
+#Handle Sends.
+events.on(
+    "send",
+    proc (send: Send) =
+        #Print the message info.
+        echo "Adding a new Send."
+        echo "From:   " & send.getSender()
+        echo "To:     " & send.getOutput()
+        echo "Amount: " & $send.getAmount()
+        echo "\r\n"
+
+        #Print before-balance, if the Lattice accepts it, and the new balance.
+        echo "Balance of " & send.getSender() & ":     " & $lattice.getBalance(send.getSender())
+        echo "Adding: " &
+            $lattice.add(
+                send
+            )
+        echo "New balance of " & send.getSender() & ": " & $lattice.getBalance(send.getSender())
+)
+
+#Handle Receives.
+events.on(
+    "recv",
+    proc (recv: Receive) =
+        #Print the message info.
+        echo "Adding a new Receive."
+        echo "From:   " & recv.getInputAddress()
+        echo "To:     " & recv.getSender()
+        echo "\r\n"
+
+        #Print before-balance, if the Lattice accepts it, and the new balance.
+        echo "Balance of " & recv.getSender() & ":     " & $lattice.getBalance(recv.getSender())
+        echo "Adding: " &
+            $lattice.add(
+                recv
+            )
+        echo "New balance of " & recv.getSender() & ": " & $lattice.getBalance(recv.getSender()) & "\r\n"
+)
+
+#Start listening.
+network.listen()
 
 runForever()
