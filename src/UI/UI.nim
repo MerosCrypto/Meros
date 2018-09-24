da7a57bffb3614e3969daa37bb8aadd0866d22bf
Luke Parker <lukep@digispend.io>
2018-09-24 14:14:42 -0500

    Expand the UI file.

--- src/UI/UI.nim
+++ src/UI/UI.nim
@@ -1,3 +1,20 @@
+#Import WebView.
 import webview
 
-open("Ember Core", "")
+#UI.
+type UI* = ref object of RootObj
+    webview: WebView
+
+#Constructor.
+proc newUI*(width: int, height:  int): UI {.raises: [Exception].} =
+    result = UI(
+        webview: newWebView("Ember Core", "", width, height)
+    )
+
+#Run function.
+proc run*(ui: UI) {.raises: [].} =
+    ui.webview.run()
+
+#Destructor.
+proc destroy*(ui: UI) {.raises: [].} =
+    ui.webview.terminate()
