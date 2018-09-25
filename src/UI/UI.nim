08e463d0266b8baaaf5f06a26370d98d08fa4200
Luke Parker <lukep@digispend.io>
2018-09-25 18:41:41 -0500

    Load static HTML data into the WebView.

--- src/UI/UI.nim
+++ src/UI/UI.nim
@@ -1,15 +1,39 @@
-#Import WebView.
+#Events lib.
+import ec_events
+
+#WebView.
 import webview
+
+#String utils standard lib.
+import strutils
+
+#Constants of the HTML/CSS/JS.
+const
+    INDEX: string = staticRead("static/index.html")
 
 #UI.
 type UI* = ref object of RootObj
+    events: EventEmitter
     webview: WebView
 
 #Constructor.
-proc newUI*(width: int, height:  int): UI {.raises: [Exception].} =
+proc newUI*(events: EventEmitter, width: int, height:  int): UI {.raises: [Exception].} =
     result = UI(
-        webview: newWebView("Ember Core", "", width, height)
+        events: events,
+        webview: newWebView(
+            "Ember Core",
+            "",
+            width,
+            height
+        )
     )
+
+    if result.webview.eval(
+        "document.write(\r\n" &
+        "    \"" & INDEX.splitLines().join("\" +\r\n    \"") & "\"" &
+        "\r\n);"
+    ) != 0:
+        raise newException(Exception, "Couldn't evaluate JS in the WebView.")
 
 #Run function.
 proc run*(ui: UI) {.raises: [].} =
