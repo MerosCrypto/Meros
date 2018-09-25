b5f92bda3e76c856da0020da5c12e94b2db74dd3
Kolby Moroz <31669092+mrmetech@users.noreply.github.com>
2018-09-24 18:10:29 -0700

    Remove dead Merit.

--- src/Database/Merit/State.nim
+++ src/Database/Merit/State.nim
@@ -18,15 +18,22 @@ proc getBalance*(state: State, account: string): BN {.raises: [ValueError].} =
     if state.hasKey(account):
         result = state[account][]
 
-proc processBlock*(state: State, newBlock: Block) {.raises: [ValueError].} =
+proc processBlock*(state: State, blockchain: Blockchain, newBlock: Block) {.raises: [ValueError].} =
+
     let miners: seq[tuple[miner: string, amount: int]] = newBlock.miners
 
     for miner in miners:
         state[miner.miner] = (state.getBalance(miner.miner) + newBN(miner.amount)).toRef()
 
+    if blockchain.height >= newBN(50001):
+        let deadMiners: seq[tuple[miner: string, amount: int]] = blockchain.blocks[^50001].miners   
+        for miner in deadMiners:
+            state[miner.miner] = (state.getBalance(miner.miner) - newBN(miner.amount)).toRef()
+
 proc processBlockchain*(state: State, blockchain: Blockchain) {.raises: [ValueError].} =
     for i in blockchain.blocks:
-        state.processBlock(i)
+        state.processBlock(blockchain, i)
+
 
 proc newState*(blockchain: Blockchain): State {.raises: [ValueError].} =
     result = newState()
