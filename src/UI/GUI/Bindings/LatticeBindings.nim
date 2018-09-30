76cb8011ed0a1dd162f949bd221038602d25ab14
Luke Parker <lukep@digispend.io>
2018-09-29 21:54:52 -0500

    Delete the GUI's direct interactions with the Lattice.
    These will be rewritten using a Channel/the async RPC.

--- src/UI/GUI/Bindings/LatticeBindings.nim
+++ src/UI/GUI/Bindings/LatticeBindings.nim
@@ -18,7 +18,7 @@ import ec_events
 import finals
 
 #WebView lib.
-import webview
+import ec_webview
 
 #String utils standard lib.
 import strutils
@@ -29,7 +29,7 @@ proc addTo*(gui: GUI) {.raises: [Exception].} =
     gui.webview.bindProc(
         "Lattice",
         "send",
-        proc (dataArg: string) {.raises: [ValueError, FinalAttributeError, Exception].} =
+        proc (dataArg: string) {.raises: [].} =
             var
                 #Split the data.
                 data: seq[string] = dataArg.split(" ")
@@ -37,33 +37,15 @@ proc addTo*(gui: GUI) {.raises: [Exception].} =
                 destination: string = data[0]
                 amount: string = data[1]
                 nonce: string = data[2]
-                #Create the Send.
-                send: Send = newSend(
-                    destination,
-                    newBN(amount),
-                    newBN(nonce)
-                )
-            #Mine the Send.
-            send.mine("aa".repeat(64).toBN(16))
-            #Sign the Send.
-            if not gui.wallet.sign(send):
-                raise newException(ValueError, "Failed to sign the GUI's Send.")
 
-            #Add it to the Lattice.
-            if not gui.events.get(
-                proc (send: Send): bool,
-                "send"
-            )(
-                send
-            ):
-                raise newException(ValueError, "Failed to add the GUI's Send.")
+            echo dataArg
     )
 
     #Receive.
     gui.webview.bindProc(
         "Lattice",
         "receive",
-        proc (dataArg: string) {.raises: [ValueError, FinalAttributeError, Exception].} =
+        proc (dataArg: string) {.raises: [].} =
             var
                 #Split the data.
                 data: seq[string] = dataArg.split(" ")
@@ -71,21 +53,6 @@ proc addTo*(gui: GUI) {.raises: [Exception].} =
                 sender: string = data[0]
                 inputNonce: BN = newBN(data[1])
                 nonce: BN = newBN(data[2])
-                #Create the Receive.
-                recv: Receive = newReceive(
-                    sender,
-                    inputNonce,
-                    nonce
-                )
-            #Sign the Receive.
-            gui.wallet.sign(recv)
 
-            #Add it to the Lattice.
-            if not gui.events.get(
-                proc (recv: Receive): bool,
-                "recv"
-            )(
-                recv
-            ):
-                raise newException(ValueError, "Failed to add the GUI's Receive.")
+            echo dataArg
     )
