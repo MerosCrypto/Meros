605af5373855c87ddd7a5f204d91d1b6c9a40c79
Luke Parker <25259837+kayabaNerve@users.noreply.github.com>
2018-12-12 23:02:25 -0600

    Merkle Trees (#43) (#44)
    Redid the Merkle Tree lib and allowed appending hashes to trees.

--- src/lib/Merkle.nim
+++ src/lib/Merkle.nim
@@ -1,111 +1,106 @@
+#Util lib.
+import Util
+
 #Hash lib.
 import Hash
 
-#Finals lib.
-import finals
+#Merkle Object.
+type Merkle* = ref object of RootObj
+    case isLeaf*: bool
+        of true:
+            discard
+        of false:
+            left*: Merkle
+            right*: Merkle
+    hash*: string
 
-#Object definitions.
-finalsd:
-    type
-        #Leaf. The lowest type on the tree.
-        Leaf* = ref object of RootObj
-            isLeaf* {.final.}: bool
-            hash* {.final.}: SHA512Hash
-
-        #Branch. Everything from the leaves to the top.
-        Branch* = ref object of Leaf
-            empty* {.final.}: bool
-            left* {.final.}: Leaf
-            right* {.final.}: Leaf
-
-#MerkleTree. The master object.
-type MerkleTree* = ref object of Branch
-
-#Lead constructor.
-func newLeafObject*(hash: SHA512Hash): Leaf {.raises: [].} =
-    result = Leaf(
+#Merkle constructor.
+func newMerkle(hash: string): Merkle {.raises: [].} =
+    Merkle(
         isLeaf: true,
         hash: hash
     )
-    result.ffinalizeIsLeaf()
-    result.ffinalizeHash()
 
-#Branch constructor.
-proc newBranchObject*(
-    left: Leaf,
-    right: Leaf,
-    empty = false
-): Branch {.raises: [].} =
-    result = Branch(
+#Rehashes a Merkle Tree.
+proc rehash(tree: Merkle) {.raises: [].} =
+    #If this is a Leaf, its hash is constant.
+    if tree.isLeaf:
+        return
+
+    #If the left tree is nil, meaning this is an empty tree...
+    if tree.left.isNil:
+        tree.hash = "".pad(64)
+    #If there's an odd number of children, duplicate the left one.
+    elif tree.right.isNil:
+        tree.hash = SHA512(tree.left.hash & tree.left.hash).toString()
+    #Hash the left & right hashes.
+    else:
+        tree.hash = SHA512(tree.left.hash & tree.right.hash).toString()
+
+#Merkle constructor based on two other Merkles.
+proc newMerkle(left: Merkle, right: Merkle): Merkle {.raises: [].} =
+    result = Merkle(
         isLeaf: false,
         left: left,
         right: right
     )
-    result.ffinalizeIsLeaf()
-    result.ffinalizeLeft()
-    result.ffinalizeRight()
+    result.rehash()
 
-    if empty:
-        result.empty = true
-        result.hash = SHA512("")
+#Opposite of isLeaf.
+func isBranch(tree: Merkle): bool {.raises: [].} =
+    return not tree.isLeaf
+
+#Checks if this tree has any duplicated entries, anywhere.
+func isFull(tree: Merkle): bool {.raises: [].} =
+    if tree.isLeaf:
+        return true
+
+    if tree.right.isNil:
+        return false
+
+    return tree.left.isFull and tree.right.isFull
+
+#Returns the deptch of the tree.
+func depth(tree: Merkle): int {.raises: [].} =
+    if tree.isLeaf:
+        return 1
+
+    #We use tree.left because the left tree will alaways be non-nil.
+    return 1 + tree.left.depth
+
+#Creates a Merkle Tree out of a single hash, filling in duplicates.
+proc chainOfDepth(depth: int, hash: string): Merkle {.raises: [].} =
+    if depth == 1:
+        return newMerkle(hash)
+    return newMerkle(chainOfDepth(depth - 1, hash), nil)
+
+#Adds a hash to a Merkle Tree.
+proc add*(tree: var Merkle, hash: string) {.raises: [].} =
+    if tree.left.isNil:
+        tree = newMerkle(
+            newMerkle(hash),
+            nil
+        )
+    elif tree.isFull:
+        tree = newMerkle(tree, chainOfDepth(tree.depth, hash))
+    elif tree.left.isBranch and not tree.left.isFull:
+        tree.left.add(hash)
+    elif tree.right.isNil:
+        tree.right = chainOfDepth(tree.depth - 1, hash)
     else:
-        result.hash = SHA512(
-            left.hash.toString() & right.hash.toString()
-        )
+        tree.right.add(hash)
 
-    result.ffinalizeEmpty()
-    result.ffinalizeHash()
+    tree.rehash()
 
-#Create a Markle Tree.
-proc newMerkleTree*(
-    hashesArg: seq[SHA512Hash]
-): MerkleTree {.raises: [].} =
-    var
-        #Extract the hashes from its arg.
-        hashes: seq[SHA512Hash] = hashesArg
-        #Create a seq of the branches.
-        branches: seq[Branch] = @[]
-        #Create a left/right leaf.
-        left: Leaf
-        right: Leaf
-
-    #If no hashes were passed in...
+#Merkle constructor based on a seq or array of hashes (as strings).
+proc newMerkle*(hashes: varargs[string]): Merkle {.raises: [].} =
+    #If there were no hashes, create a nil tree.
     if hashes.len == 0:
-        return cast[MerkleTree](
-            newBranchObject(
-                newLeafObject(SHA512("")),
-                newLeafObject(SHA512("")),
-                true
-            )
-        )
+        return newMerkle(nil, nil)
 
-    #Make sure we have an even amount of branches.
-    if (hashes.len mod 2) == 1:
-        hashes.add(hashes[hashes.len-1])
-
-    #Iterate over the hashes.
-    for i, hash in hashes:
-        #If this is a left branch, use the Left leaf.
-        if (i mod 2) == 0:
-            left = newLeafObject(hash)
-        else:
-            #If this is a Right branch, use the Right leaf.
-            right = newLeafObject(hash)
-            #Now that we have a left and a right leaf, make a Branch.
-            branches.add(newBranchObject(left, right))
-
-    #While there's branches...
-    while branches.len != 1:
-        #If the branches are no longer even, add a copy of the last one.
-        if (branches.len mod 2) == 1:
-            branches.add(branches[branches.len-1])
-
-        #Iterate through each pair of branches...
-        for i in 0 ..< int(branches.len / 2):
-            #Set the branch (on the left half of the seq) to involve two of the branches ahead of it.
-            branches[i] = newBranchObject(branches[i * 2], branches[(i * 2) + 1])
-        #Cut off the right half of the seq.
-        branches.setLen((int) branches.len / 2)
-
-    #Set the Result to the remaining branch.
-    result = cast[MerkleTree](branches[0])
+    result = newMerkle(
+        newMerkle(hashes[0]),
+        nil
+    )
+    for hash in hashes[1 ..< hashes.len]:
+        result.add(hash)
