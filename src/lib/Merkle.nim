029a4e6911aef0a2c60f9a09ac9f37ead8a971a7
Eli <elimaynard923@gmail.com>
2018-12-12 21:54:40 -0500

    Merkle more like mermaid

--- src/lib/Merkle.nim
+++ src/lib/Merkle.nim
@@ -1,104 +1,115 @@
 #Hash lib.
 import Hash
 
-#Finals lib.
-import finals
-
 #Object definitions.
-finalsd:
-    type
-        #Leaf. The lowest type on the tree.
-        Leaf* = ref object of RootObj
-            isLeaf* {.final.}: bool
-            hash* {.final.}: SHA512Hash
+type Merkle* = ref object
+    case isLeaf*: bool
+    of true:
+        discard
+    of false:
+        left*: Merkle
+        right*: Merkle
 
-        #Branch. Everything from the leaves to the top.
-        Branch* = ref object of Leaf
-            empty* {.final.}: bool
-            left* {.final.}: Leaf
-            right* {.final.}: Leaf
+    hash*: string
 
-#MerkleTree. The master object.
-type MerkleTree* = ref object of Branch
+func isBranch(tree: Merkle): bool {.raises: [].} =
+    return not tree.isLeaf
 
-#Lead constructor.
-func newLeafObject*(hash: SHA512Hash): Leaf {.raises: [].} =
-    result = Leaf(
-        isLeaf: true,
-        hash: hash
-    )
+func depth(tree: Merkle): int {.raises: [].} =
+    if tree.isLeaf:
+        return 1
+    else:
+        return 1 + tree.left.depth
 
-#Branch constructor.
-proc newBranchObject*(
-    left: Leaf,
-    right: Leaf,
-    empty = false
-): Branch {.raises: [].} =
-    result = Branch(
-        isLeaf: false,
-        left: left,
-        right: right
-    )
+func newMerkle(hash: string): Merkle {.raises: [].} =
+    result = Merkle(isLeaf: true, hash: hash)
 
-    if empty:
-        result.empty = true
-        result.hash = SHA512("")
+proc reHash(tree: Merkle) {.raises: [].} =
+    if tree.isLeaf:
         return
+    if tree.right.isNil:
+        tree.hash = SHA512(tree.left.hash & tree.left.hash).toString
+    else:
+        tree.hash = SHA512(tree.left.hash & tree.right.hash).toString
 
-    result.hash = SHA512(
-        left.hash.toString() & right.hash.toString()
-    )
+proc newMerkle(left: Merkle, right: Merkle): Merkle {.raises: [].} =
+    result = Merkle(isLeaf: false, hash: "", left: left, right: right)
+    result.reHash()
 
-#Create a Markle Tree.
-proc newMerkleTree*(
-    hashesArg: seq[SHA512Hash]
-): MerkleTree {.raises: [].} =
-    var
-        #Extract the hashes from its arg.
-        hashes: seq[SHA512Hash] = hashesArg
-        #Create a seq of the branches.
-        branches: seq[Branch] = @[]
-        #Create a left/right leaf.
-        left: Leaf
-        right: Leaf
+func isFull(tree: Merkle): bool {.raises: [].} =
+    if tree.isLeaf:
+        return true
 
-    #If no hashes were passed in...
+    if tree.right.isNil:
+        return false
+
+    return tree.left.isFull and tree.right.isFull
+
+func `$`(tree: Merkle): string {.raises: [].} =
+    if tree.isLeaf:
+        return tree.hash
+    elif tree.right.isNil:
+        return "(" & $tree.left & ", nil)"
+    else:
+        return "(" & $tree.left & ", " & $tree.right & ")"
+
+proc chainOfDepth(depth: int, hash: string): Merkle {.raises: [].} =
+    assert depth >= 1
+    if depth == 1:
+        return newMerkle(hash)
+    else:
+        return newMerkle(chainOfDepth(depth - 1, hash), nil)
+
+proc add*(tree: var Merkle, hash: string) {.raises: [].} =
+    if tree.isFull:
+        let sibling = chainOfDepth(tree.depth, hash)
+        let parent = newMerkle(tree, sibling)
+        tree = parent
+    elif tree.left.isBranch and not tree.left.isFull:
+        tree.left.add(hash)
+    elif tree.right.isNil:
+        tree.right = chainOfDepth(tree.depth - 1, hash)
+    else:
+        tree.right.add(hash)
+
+    tree.reHash()
+
+proc newMerkle*(hashes: varargs[string]): Merkle =
     if hashes.len == 0:
-        return cast[MerkleTree](
-            newBranchObject(
-                newLeafObject(SHA512("")),
-                newLeafObject(SHA512("")),
-                true
-            )
-        )
+        raise IndexError.newException("Merkle trees require 1 or more hashes")
 
-    #Make sure we have an even amount of branches.
-    if (hashes.len mod 2) == 1:
-        hashes.add(hashes[hashes.len-1])
+    result = newMerkle(hashes[0])
+    for hash in hashes[1 ..< hashes.len]:
+        result.add(hash)
 
-    #Iterate over the hashes.
-    for i, hash in hashes:
-        #If this is a left branch, use the Left leaf.
-        if (i mod 2) == 0:
-            left = newLeafObject(hash)
-        else:
-            #If this is a Right branch, use the Right leaf.
-            right = newLeafObject(hash)
-            #Now that we have a left and a right leaf, make a Branch.
-            branches.add(newBranchObject(left, right))
+when isMainModule:
+    proc hash(s1, s2: string): string =
+        SHA512(s1 & s2).toString
 
-    #While there's branches...
-    while branches.len != 1:
-        #If the branches are no longer even, add a copy of the last one.
-        if (branches.len mod 2) == 1:
-            branches.add(branches[branches.len-1])
+    assert(newMerkle("a", "b", "c", "d").hash == hash(hash("a", "b"), hash("c", "d")) )
 
-        #Iterate through each pair of branches...
-        for i in 0 ..< int(branches.len / 2):
-            #Set the branch (on the left half of the seq) to involve two of the branches ahead of it.
-            branches[i] = newBranchObject(branches[i * 2], branches[(i * 2) + 1])
-        #Cut off the right half of the seq.
-        branches.setLen((int) branches.len / 2)
+    assert(newMerkle("1", "2", "3", "4", "5", "6", "7", "8").hash ==
+            hash(hash(hash("1", "2"), hash("3", "4")), hash(hash("5", "6"), hash("7", "8"))) )
 
-    #Set the Result to the remaining branch.
-    result = cast[MerkleTree](branches[0])
+    assert(newMerkle("1", "2", "3").hash == hash(hash("1", "2"), hash("3", "3")) )
+
+    block:
+        var m = newMerkle("naughty")
+        assert m.hash == "naughty"
+
+        m.add("children")
+        let h1 = hash("naughty", "children")
+        assert m.hash == h1
+
+        m.add("get")
+        assert m.hash == hash(h1, hash("get", "get"))
+
+        m.add("coal")
+        let h2 = hash(h1, hash("get", "coal"))
+        assert m.hash == h2
+
+        m.add("for")
+        assert m.hash == hash(h2, hash(hash("for", "for"), hash("for", "for")))
+
+        m.add("christmas")
+        assert m.hash == hash(h2, hash(hash("for", "christmas"), hash("for", "christmas")))
