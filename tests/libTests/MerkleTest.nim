0942699e6214a011a005bf99210757706862dbbd
Luke Parker <25259837+kayabaNerve@users.noreply.github.com>
2018-12-12 22:42:50 -0600

    Update MerkleTest.nim

--- tests/libTests/MerkleTest.nim
+++ tests/libTests/MerkleTest.nim
@@ -1,45 +1,57 @@
 #Merkle Tree Test.
 
+#Util lib.
+import ../../src/lib/Util
+
 #Hash lib.
 import ../../src/lib/Hash
+
+#Base lib.
+import ../../src/lib/Base
 
 #Merkle lib.
 import ../../src/lib/Merkle
 
-#Util lib
-import ../../src/lib/Util
+var
+    #First leaf.
+    a: string = SHA512("a").toString()
+    #Second leaf.
+    b: string = SHA512("b").toString()
+    #Third leaf.
+    c: string = SHA512("c").toString()
 
-proc hash(s1, s2: string): string =
-    SHA512(s1 & s2).toString
+    #First hash.
+    ab: string = SHA512(
+        a & b
+    ).toString()
+    #Second hash.
+    cc: string = SHA512(
+        c & c
+    ).toString()
+    #Root hash.
+    hash: string = SHA512(
+        ab & cc
+    ).toString()
 
+    #Create the Merkle Tree.
+    merkle: Merkle = newMerkle(a, b, c)
+
+#Test the results.
+assert(hash == merkle.hash, "Merkle hash is not what it should be.")
+
+#Test nil Merle trees.
 assert(newMerkle().hash == "".pad(64))
 
-assert(newMerkle(["a", "b", "c", "d"]).hash == hash(hash("a", "b"), hash("c", "d")) )
+#Test adding hashes.
+merkle = newMerkle(a)
+merkle.add(b)
+merkle.add(c)
+assert(merkle.hash == hash)
 
-assert(newMerkle(["1", "2", "3", "4", "5", "6", "7", "8"]).hash ==
-        hash(hash(hash("1", "2"), hash("3", "4")), hash(hash("5", "6"), hash("7", "8"))) )
+merkle = newMerkle()
+merkle.add(a)
+merkle.add(b)
+merkle.add(c)
+assert(merkle.hash == hash)
 
-assert(newMerkle(["1", "2", "3"]).hash == hash(hash("1", "2"), hash("3", "3")) )
-
-block:
-    var m = newMerkle(["naughty"])
-    assert m.hash == "naughty"
-
-    m.add("children")
-    let h1 = hash("naughty", "children")
-    assert m.hash == h1
-
-    m.add("get")
-    assert m.hash == hash(h1, hash("get", "get"))
-
-    m.add("coal")
-    let h2 = hash(h1, hash("get", "coal"))
-    assert m.hash == h2
-
-    m.add("for")
-    assert m.hash == hash(h2, hash(hash("for", "for"), hash("for", "for")))
-
-    m.add("christmas")
-    assert m.hash == hash(h2, hash(hash("for", "christmas"), hash("for", "christmas")))
-
-echo "Finished the lib/MerkleTree test."
+echo "Finished the lib/Merkle test."
