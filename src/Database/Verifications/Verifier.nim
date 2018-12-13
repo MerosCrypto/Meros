a53fe65cec8bfa6cbef77c6a3694fe1314602767
Luke Parker <lukep@embercrypto.io>
2018-12-12 18:50:08 -0600

    Move calculateSig/verify out of the object file.

---
+++ src/Database/Verifications/Verifier.nim
@@ -0,0 +1,49 @@
+#Errors lib.
+import ../../lib/Errors
+
+#Hash lib.
+import ../../lib/Hash
+
+#BLS lib.
+import ../../lib/BLS
+
+#Finals lib.
+import finals
+
+#Calculate the aggregate signature.
+proc calculateSig*(verifs: seq[Verification]): BLSSignature {.raises: [BLSError].} =
+    #If there's no verifications...
+    if verifs.len == 0:
+        #Set a 0'd out signature.
+        try:
+            verifs.aggregate = newBLSSignature(char(0).repeat(96))
+        except:
+            raise newException(BLSError, "Couldn't aggregate the signature for the Verifications.")
+        return
+
+    #Declare a seq for the Signatures.
+    var sigs: seq[BLSSignature]
+    #Put every signature in the seq.
+    for verif in verifs:
+        sigs.add(verif.signature)
+    #Set the aggregate.
+    result = sigs.aggregate()
+
+#Verify the aggregate signature.
+proc verify*(verifs: seq[Verification], sig: BLSSignature): bool {.raises: [BLSError].} =
+    #If there's no verifications...
+    if verifs.len == 0:
+        return (sig == newBLSSignature(char(0).repeat(96)))
+
+    #Create the Aggregation Infos.
+    var agInfos: seq[BLSAggregationInfo] = @[]
+    for verif in verifs:
+        agInfos.add(
+            newBLSAggregationInfo(verif.verifier, verif.hash.toString())
+        )
+
+    #Add the aggregated Aggregation Infos to the signature.
+    sig.setAggregationInfo(agInfos.aggregate())
+
+    #Verify the signature.
+    result = sig.verify()
--- src/Database/Verifications/objects/VerifierObj.nim
+++ src/Database/Verifications/objects/VerifierObj.nim
@@ -1,11 +1,5 @@
-#Errors lib.
-import ../../../lib/Errors
-
-#Hash lib.
-import ../../../lib/Hash
-
-#BLS lib.
-import ../../../lib/BLS
+#Verification object.
+import VerificationObj
 
 #Finals lib.
 import finals
@@ -17,45 +11,14 @@ finalsd:
         key* {.final.}: string
         #Verifier height.
         height*: uint
+        #Amount of Verifications which have been archived.
+        archived*: uint
         #seq of the Verifications.
         verifications*: seq[Verification]
 
-discard """
-#Calculate the aggregate signature.
-proc calculateSig*(verifs: Verifications) {.raises: [BLSError].} =
-    #If there's no verifications...
-    if verifs.verifications.len == 0:
-        #Set a 0'd out signature.
-        try:
-            verifs.aggregate = newBLSSignature(char(0).repeat(96))
-        except:
-            raise newException(BLSError, "Couldn't aggregate the signature for the Verifications.")
-        return
+# [] operators.
+func `[]`(verifier: Verifier, index: int): Verification {.raises: [].} =
+    verifier.verifications[index]
 
-    #Declare a seq for the Signatures.
-    var sigs: seq[BLSSignature]
-    #Put every signature in the seq.
-    for verif in verifs.verifications:
-        sigs.add(verif.signature)
-    #Set the aggregate.
-    verifs.aggregate = sigs.aggregate()
-
-#Verify the aggregate signature.
-proc verify*(verifs: Verifications): bool {.raises: [BLSError].} =
-    #If there's no verifications...
-    if verifs.verifications.len == 0:
-        return true
-
-    #Create the Aggregation Infos.
-    var agInfos: seq[BLSAggregationInfo] = @[]
-    for verif in verifs.verifications:
-        agInfos.add(
-            newBLSAggregationInfo(verif.verifier, verif.hash.toString())
-        )
-
-    #Add the aggregated Aggregation Infos to the signature.
-    verifs.aggregate.setAggregationInfo(agInfos.aggregate())
-
-    #Verify the signature.
-    result = verifs.aggregate.verify()
-"""
+func `[]`(verifier: Verifier, slice: Slice[int]): seq[Verification] {.raises: [].} =
+    verifier.verifications[slice]
