146e2091da91dbc028dfad654f5b8cb15e4b8551
Quelklef <elimaynard923@gmail.com>
2019-01-20 23:05:00 -0500

    Faster newMerkle() and withoutNLeaves (#48)

--- src/lib/Merkle.nim
+++ src/lib/Merkle.nim
@@ -4,6 +4,9 @@ import Util
 #Hash lib.
 import Hash
 
+#Math lib
+import math
+
 #Merkle Object.
 type Merkle* = ref object of RootObj
     case isLeaf*: bool
@@ -20,6 +23,14 @@ func newMerkle(hash: string): Merkle {.raises: [].} =
         isLeaf: true,
         hash: hash
     )
+
+#tostring
+proc `$`*(tree: Merkle): string =
+    if tree.isNil:
+        return "_"
+    if tree.isLeaf:
+        return tree.hash
+    return "M(" & $tree.left & ", " & $tree.right & ")"
 
 #Rehashes a Merkle Tree.
 proc rehash(tree: Merkle) {.raises: [].} =
@@ -63,20 +74,30 @@ func isFull(tree: Merkle): bool {.raises: [].} =
 #Returns the deptch of the tree.
 func depth(tree: Merkle): int {.raises: [].} =
     if tree.isLeaf:
-        return 1
+        return 0
 
     #We use tree.left because the left tree will alaways be non-nil.
     return 1 + tree.left.depth
 
+#Number of leaves
+func leafCount(tree: Merkle): int {.raises: [].} =
+    if tree.isNil:
+        return 0
+    if tree.isLeaf:
+        return 1
+    return tree.left.leafCount + tree.right.leafCount
+
 #Creates a Merkle Tree out of a single hash, filling in duplicates.
 proc chainOfDepth(depth: int, hash: string): Merkle {.raises: [].} =
-    if depth == 1:
+    if depth == 0:
         return newMerkle(hash)
     return newMerkle(chainOfDepth(depth - 1, hash), nil)
 
 #Adds a hash to a Merkle Tree.
 proc add*(tree: var Merkle, hash: string) {.raises: [].} =
-    if tree.left.isNil:
+    if tree.isLeaf:
+        tree = newMerkle(tree, newMerkle(hash))
+    elif tree.left.isNil:
         tree = newMerkle(
             newMerkle(hash),
             nil
@@ -92,15 +113,74 @@ proc add*(tree: var Merkle, hash: string) {.raises: [].} =
 
     tree.rehash()
 
+#From https://stackoverflow.com/a/15327567/4608364
+proc ceilLog2(x: uint64): uint64 =
+    const t: array[6, uint64] = [
+        0xFFFFFFFF00000000'u64,
+        0x00000000FFFF0000'u64,
+        0x000000000000FF00'u64,
+        0x00000000000000F0'u64,
+        0x000000000000000C'u64,
+        0x0000000000000002'u64,
+    ]
+    var x: uint64 = x
+    var y: uint64 = if (x and (x - 1)) == 0: 0 else: 1
+    var j: uint64 = 32
+    for i in 0 ..< 6:
+        let k: uint64 = if (x and t[i]) == 0'u64: 0'u64 else: j
+        y += k
+        x = x shr k
+        j = j shr 1
+    return y
+
+#forward declaration
+proc newMerkleAux(hashes: openarray[string], targetDepth: int): Merkle {.raises: [].}
+
 #Merkle constructor based on a seq or array of hashes (as strings).
 proc newMerkle*(hashes: varargs[string]): Merkle {.raises: [].} =
     #If there were no hashes, create a nil tree.
     if hashes.len == 0:
         return newMerkle(nil, nil)
 
-    result = newMerkle(
-        newMerkle(hashes[0]),
-        nil
-    )
-    for hash in hashes[1 ..< hashes.len]:
-        result.add(hash)
+    let targetDepth = int(ceilLog2(uint64(len(hashes))))
+    return newMerkleAux(hashes, targetDepth)
+
+proc newMerkleAux(hashes: openarray[string], targetDepth: int): Merkle {.raises: [].} =
+    if targetDepth == 0:
+        assert(len(hashes) == 1)
+        return newMerkle(hashes[0])
+
+    #half of the number of items in the lowest level of the tree (if it were completely filled)
+    let halfWidth = 2^(targetDepth-1)
+
+    if len(hashes) <= halfWidth:
+        # we need to duplicate LHS on RHS
+        return newMerkle(newMerkleAux(hashes, targetDepth - 1), nil)
+    else:
+        let lhs = newMerkleAux(hashes[0 ..< halfWidth], targetDepth - 1)
+        let rhs = newMerkleAux(hashes[halfWidth ..< len(hashes)], targetDepth - 1)
+        return newMerkle(lhs, rhs)
+
+
+#forward declaration
+proc withoutNLeavesAux(tree: Merkle, n: int): Merkle {.raises: [].}
+
+#Nonmutatively remove the last N leaves from a tree
+proc withoutNLeaves*(tree: Merkle, n: int): Merkle {.raises: [].} =
+    #resize
+    var tree = tree
+    var n = n
+    #continually chop of entire right half while we can
+    while n > 0 and n >= tree.right.leafCount:
+        n -= tree.right.leafCount
+        tree = tree.left
+    return tree.withoutNLeavesAux(n)
+
+proc withoutNLeavesAux(tree: Merkle, n: int): Merkle {.raises: [].} =
+    if n == 0:
+        return tree
+
+    if n >= tree.right.leafCount:
+        return newMerkle(tree.left.withoutNLeavesAux(n - tree.right.leafCount), nil)
+    else:
+        return newMerkle(tree.left, tree.right.withoutNLeavesAux(n))
--- tests/libTests/MerkleTest.nim
+++ tests/libTests/MerkleTest.nim
@@ -12,46 +12,134 @@ import ../../src/lib/Base
 #Merkle lib.
 import ../../src/lib/Merkle
 
-var
-    #First leaf.
-    a: string = SHA512("a").toString()
-    #Second leaf.
-    b: string = SHA512("b").toString()
-    #Third leaf.
-    c: string = SHA512("c").toString()
+block:
+    var
+        #First leaf.
+        a: string = SHA512("a").toString()
+        #Second leaf.
+        b: string = SHA512("b").toString()
+        #Third leaf.
+        c: string = SHA512("c").toString()
 
-    #First hash.
-    ab: string = SHA512(
-        a & b
-    ).toString()
-    #Second hash.
-    cc: string = SHA512(
-        c & c
-    ).toString()
-    #Root hash.
-    hash: string = SHA512(
-        ab & cc
-    ).toString()
+        #First hash.
+        ab: string = SHA512(
+            a & b
+        ).toString()
+        #Second hash.
+        cc: string = SHA512(
+            c & c
+        ).toString()
+        #Root hash.
+        hash: string = SHA512(
+            ab & cc
+        ).toString()
 
-    #Create the Merkle Tree.
-    merkle: Merkle = newMerkle(a, b, c)
+        #Create the Merkle Tree.
+        merkle: Merkle = newMerkle(a, b, c)
 
-#Test the results.
-assert(hash == merkle.hash, "Merkle hash is not what it should be.")
+    #Test the results.
+    assert(hash == merkle.hash, "Merkle hash is not what it should be.")
 
-#Test nil Merle trees.
-assert(newMerkle().hash == "".pad(64))
+    #Test nil Merle trees.
+    assert(newMerkle().hash == "".pad(64))
 
-#Test adding hashes.
-merkle = newMerkle(a)
-merkle.add(b)
-merkle.add(c)
-assert(merkle.hash == hash)
+    #Test adding hashes.
+    merkle = newMerkle(a)
+    merkle.add(b)
+    merkle.add(c)
+    assert(merkle.hash == hash)
 
-merkle = newMerkle()
-merkle.add(a)
-merkle.add(b)
-merkle.add(c)
-assert(merkle.hash == hash)
+    merkle = newMerkle()
+    merkle.add(a)
+    merkle.add(b)
+    merkle.add(c)
+    assert(merkle.hash == hash)
+
+block:
+    let
+        a = SHA512("a").toString()
+        b = SHA512("b").toString()
+        c = SHA512("c").toString()
+        d = SHA512("d").toString()
+
+        e = SHA512("e").toString()
+        f = SHA512("f").toString()
+        g = SHA512("g").toString()
+        h = SHA512("h").toString()
+
+        #will test trees comprised of a-e, a-f, a-g, and a-h
+
+        ab = SHA512(a & b).toString()
+        cd = SHA512(c & d).toString()
+        abcd = SHA512(ab & cd).toString()
+
+    #first, a-e
+    let
+        ee = SHA512(e & e).toString()
+        eeee = SHA512(ee & ee).toString()
+        abcdeeee = SHA512(abcd & eeee).toString()
+        tree_ae = newMerkle(a, b, c, d, e)
+    assert(tree_ae.hash == abcdeeee)
+
+    #now, a-f
+    let
+        ef = SHA512(e & f).toString()
+        efef = SHA512(ef & ef).toString()
+        abcdefef = SHA512(abcd & efef).toString()
+        #not just ANY tree
+        #this one is tree as FUCK
+        tree_af = newMerkle(a, b, c, d, e, f)
+    assert(tree_af.hash == abcdefef)
+
+    #now, a-g
+    let
+        gg = SHA512(g & g).toString()
+        efgg = SHA512(ef & gg).toString()
+        abcdefgg = SHA512(abcd & efgg).toString()
+        tree_ag = newMerkle(a, b, c, d, e, f, g)
+    assert(tree_ag.hash == abcdefgg)
+
+    #finally, a-h
+    let
+        gh = SHA512(g & h).toString()
+        efgh = SHA512(ef & gh).toString()
+        abcdefgh = SHA512(abcd & efgh).toString()
+        tree_ah = newMerkle(a, b, c, d, e, f, g, h)
+    assert(tree_ah.hash == abcdefgh)
+
+
+    #test leaf removal
+    let tree_ad = newMerkle(abcd)
+    let abcc = SHA512(ab & SHA512(c & c).toString()).toString()
+
+    assert(tree_ah.withoutNLeaves(7).hash == a)
+    assert(tree_ah.withoutNLeaves(6).hash == ab)
+    assert(tree_ah.withoutNLeaves(5).hash == abcc)
+    assert(tree_ah.withoutNLeaves(4).hash == tree_ad.hash)
+    assert(tree_ah.withoutNLeaves(3).hash == tree_ae.hash)
+    assert(tree_ah.withoutNLeaves(2).hash == tree_af.hash)
+    assert(tree_ah.withoutNLeaves(1).hash == tree_ag.hash)
+    assert(tree_ah.withoutNLeaves(0).hash == tree_ah.hash)
+
+    assert(tree_ag.withoutNLeaves(6).hash == a)
+    assert(tree_ag.withoutNLeaves(5).hash == ab)
+    assert(tree_ag.withoutNLeaves(4).hash == abcc)
+    assert(tree_ag.withoutNLeaves(3).hash == tree_ad.hash)
+    assert(tree_ag.withoutNLeaves(2).hash == tree_ae.hash)
+    assert(tree_ag.withoutNLeaves(1).hash == tree_af.hash)
+    assert(tree_ag.withoutNLeaves(0).hash == tree_ag.hash)
+
+    assert(tree_af.withoutNLeaves(5).hash == a)
+    assert(tree_af.withoutNLeaves(4).hash == ab)
+    assert(tree_af.withoutNLeaves(3).hash == abcc)
+    assert(tree_af.withoutNLeaves(2).hash == tree_ad.hash)
+    assert(tree_af.withoutNLeaves(1).hash == tree_ae.hash)
+    assert(tree_af.withoutNLeaves(0).hash == tree_af.hash)
+
+    assert(tree_ae.withoutNLeaves(4).hash == a)
+    assert(tree_ae.withoutNLeaves(3).hash == ab)
+    assert(tree_ae.withoutNLeaves(2).hash == abcc)
+    assert(tree_ae.withoutNLeaves(1).hash == tree_ad.hash)
+    assert(tree_ae.withoutNLeaves(0).hash == tree_ae.hash)
 
 echo "Finished the lib/Merkle test."
