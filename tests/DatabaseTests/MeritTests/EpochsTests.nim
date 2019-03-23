3c380f1c0d563640ada583f20204a245fc7866e4
Luke Parker <lukep@embercrypto.io>
2019-03-23 03:43:48 -0500

    Add an Epochs DB Test.

--- tests/DatabaseTests/MeritTests/EpochsTests.nim
+++ tests/DatabaseTests/MeritTests/EpochsTests.nim
@@ -1,6 +1,9 @@
-#Import each of the Epochs Tests.
+#Epochs Tests.
+
+import EpochsTests/EpochsTestDB
+
 import EpochsTests/EpochsTest0
 import EpochsTests/EpochsTest1
 import EpochsTests/EpochsTest2
 
-echo "Finished the Database/Merit/Epochs tests."
+echo "Finished the Database/Merit/Epochs Tests."
--- tests/DatabaseTests/MeritTests/MeritTest.nim
+++ tests/DatabaseTests/MeritTests/MeritTest.nim
@@ -1,10 +1,3 @@
 #Merit Test.
 
-discard """
-Since the Blockchain test should test the Blockchain and mining
-Since the State test should test the State handling the blockchain
-Since the EpochsTests test reward calculation
-This should test not only the processBlock function, but also the DB saving/loading.
-"""
-
-echo "The Database/Merit/Merit test is empty."
+echo "The Database/Merit/Merit Test is empty."
