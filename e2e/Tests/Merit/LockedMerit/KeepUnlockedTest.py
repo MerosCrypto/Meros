4c35b21896b2bc875e2540a7b5af5240e451f402
Luke Parker <lukeparker5132@gmail.com>
2020-07-10 06:46:54 -0500

    Expand the keepUnlocked test to verify Elements work as well.

--- e2e/Tests/Merit/LockedMerit/KeepUnlockedTest.py
+++ e2e/Tests/Merit/LockedMerit/KeepUnlockedTest.py
@@ -5,12 +5,21 @@ from e2e.Meros.RPC import RPC
 from e2e.Meros.Liver import Liver
 from e2e.Meros.Syncer import Syncer
 
+from e2e.Tests.Errors import TestError
+
 def KeepUnlockedTest(
   rpc: RPC
 ) -> None:
   file: IO[Any] = open("e2e/Vectors/Merit/LockedMerit/KeepUnlocked.json", "r")
-  chain: List[Dict[str, Any]] = json.loads(file.read())
+  chains: List[List[Dict[str, Any]]] = json.loads(file.read())
   file.close()
 
-  Liver(rpc, chain).live()
-  Syncer(rpc, chain).sync()
+  def verifyUnlocked(
+    _: int
+  ) -> None:
+    if rpc.call("merit", "getMerit", [0])["status"] != "Unlocked":
+      raise TestError("Meros didn't keep Merit unlocked.")
+
+  for chain in chains:
+    Liver(rpc, chain, everyBlock=verifyUnlocked).live()
+    Syncer(rpc, chain).sync()
--- e2e/Vectors/Generation/Merit/LockedMerit/KeepUnlocked.py
+++ e2e/Vectors/Generation/Merit/LockedMerit/KeepUnlocked.py
@@ -1,8 +1,17 @@
 from typing import IO, Any
 import json
 
+from e2e.Classes.Consensus.DataDifficulty import DataDifficulty
+
 from e2e.Vectors.Generation.PrototypeChain import PrototypeChain
 
+keepsUnlockedViaElements: PrototypeChain = PrototypeChain(1, False)
+for b in range(24):
+  keepsUnlockedViaElements.add(elements=[DataDifficulty(b, b, 0)])
+
 vectors: IO[Any] = open("e2e/Vectors/Merit/LockedMerit/KeepUnlocked.json", "w")
-vectors.write(json.dumps(PrototypeChain(25).toJSON()))
+vectors.write(json.dumps([
+  PrototypeChain(25).toJSON(),
+  keepsUnlockedViaElements.toJSON()
+]))
 vectors.close()
