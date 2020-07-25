2f62ffd24bdea70b4b6b520639ce581a59747c1e
Luke Parker <lukeparker5132@gmail.com>
2020-07-25 12:20:42 -0500

    Add system.quit to the RPC Sample.

--- samples/RPCSample.nim
+++ samples/RPCSample.nim
@@ -35,7 +35,9 @@ const ARGUMENTS: Table[string, seq[char]] = {
   "personal_getAddress":  @[],
 
   "personal_send": @['s', 's'],
-  "personal_data": @['s']
+  "personal_data": @['s'],
+
+  "system_quit": @[]
 }.toTable()
 
 var
