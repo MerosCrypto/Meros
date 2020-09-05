a80aacc16cb7c6b89444467e5c7671f602f20f29
Luke Parker <lukeparker5132@gmail.com>
2020-09-05 18:27:17 -0500

    Vectorize #238 test.

---
+++ e2e/Vectors/Generation/Transactions/Prune/TwoHundredThirtyEight.py
@@ -0,0 +1,49 @@
+from typing import IO, List, Any
+import json
+
+import ed25519
+from e2e.Libs.BLS import PrivateKey
+
+from e2e.Classes.Transactions.Data import Data
+
+from e2e.Classes.Consensus.Verification import SignedVerification
+from e2e.Classes.Consensus.VerificationPacket import VerificationPacket
+from e2e.Classes.Consensus.SpamFilter import SpamFilter
+
+from e2e.Vectors.Generation.PrototypeChain import PrototypeChain
+
+dataFilter: SpamFilter = SpamFilter(5)
+
+edPrivKey: ed25519.SigningKey = ed25519.SigningKey(b'\0' * 32)
+edPubKey: ed25519.VerifyingKey = edPrivKey.get_verifying_key()
+
+proto: PrototypeChain = PrototypeChain(40, keepUnlocked=True)
+
+datas: List[Data] = [Data(bytes(32), edPubKey.to_bytes())]
+for d in range(2):
+  datas.append(Data(datas[0].hash, d.to_bytes(1, "little")))
+for data in datas:
+  data.sign(edPrivKey)
+  data.beat(dataFilter)
+
+proto.add(1)
+proto.add(2)
+proto.add(packets=[VerificationPacket(datas[0].hash, [0, 1, 2])])
+proto.add(packets=[VerificationPacket(datas[1].hash, [0])])
+proto.add(packets=[VerificationPacket(datas[2].hash, [1])])
+
+verif: SignedVerification = SignedVerification(datas[2].hash)
+verif.sign(2, PrivateKey(2))
+
+for _ in range(5):
+  proto.add()
+
+vectors: IO[Any] = open("e2e/Vectors/Transactions/Prune/TwoHundredThirtyEight.json", "w")
+vectors.write(
+  json.dumps({
+    "blockchain": proto.toJSON(),
+    "datas": [data.toJSON() for data in datas],
+    "verification": verif.toSignedJSON()
+  })
+)
+vectors.close()
