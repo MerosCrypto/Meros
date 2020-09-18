3d160018916252caec34527c7299023e18639e83
Luke Parker <lukeparker5132@gmail.com>
2020-09-18 18:37:00 -0500

    Remove inlined vector generation for UnmentionedBeatMentioned

--- e2e/Tests/Consensus/Families/UnmentionedBeatMentionedTest.py
+++ e2e/Tests/Consensus/Families/UnmentionedBeatMentionedTest.py
@@ -1,51 +1,24 @@
-from typing import List
-
-import ed25519
-from e2e.Libs.BLS import PrivateKey
+from typing import Dict, List, Any
+import json
 
 from e2e.Classes.Transactions.Data import Data
 
 from e2e.Classes.Consensus.Verification import SignedVerification
-from e2e.Classes.Consensus.VerificationPacket import VerificationPacket
-from e2e.Classes.Consensus.SpamFilter import SpamFilter
-
-from e2e.Vectors.Generation.PrototypeChain import PrototypeChain
 
 from e2e.Meros.RPC import RPC
 from e2e.Meros.Liver import Liver
 
 from e2e.Tests.Errors import TestError
 
-dataFilter: SpamFilter = SpamFilter(5)
-
-edPrivKey: ed25519.SigningKey = ed25519.SigningKey(b'\0' * 32)
-edPubKey: ed25519.VerifyingKey = edPrivKey.get_verifying_key()
-
-proto: PrototypeChain = PrototypeChain(40, keepUnlocked=True)
-proto.add(1)
-
-datas: List[Data] = [Data(bytes(32), edPubKey.to_bytes())]
-for d in range(2):
-  datas.append(Data(datas[0].hash, d.to_bytes(1, "little")))
-for data in datas:
-  data.sign(edPrivKey)
-  data.beat(dataFilter)
-
-verif: SignedVerification = SignedVerification(datas[1].hash)
-verif.sign(0, PrivateKey(0))
-proto.add(
-  packets=[
-    VerificationPacket(datas[0].hash, [0]),
-    VerificationPacket(datas[2].hash, [1])
-  ]
-)
-
-for _ in range(5):
-  proto.add()
-
 def UnmentionedBeatMentionedTest(
   rpc: RPC
 ) -> None:
+  vectors: Dict[str, Any]
+  with open("e2e/Vectors/Consensus/Families/UnmentionedBeatMentioned.json", "r") as file:
+    vectors = json.loads(file.read())
+  datas: List[Data] = [Data.fromJSON(data) for data in vectors["datas"]]
+  verif: SignedVerification = SignedVerification.fromSignedJSON(vectors["verification"])
+
   def sendDatas() -> None:
     for d in range(len(datas)):
       if rpc.meros.liveTransaction(datas[d]) != rpc.meros.live.recv():
@@ -61,11 +34,9 @@ def UnmentionedBeatMentionedTest(
 
   Liver(
     rpc,
-    proto.toJSON(),
+    vectors["blockchain"],
     callbacks={
       41: sendDatas,
       47: verifyMentionedWon
     }
   ).live()
-
-  raise TestError("Test is unfinished; inlines vector generation which causes them to be re-mined at runtime.")
---
+++ e2e/Vectors/Generation/Consensus/Families/UnmentionedBeatMentioned.py
@@ -0,0 +1,47 @@
+from typing import List
+import json
+
+import ed25519
+from e2e.Libs.BLS import PrivateKey
+
+from e2e.Classes.Transactions.Data import Data
+
+from e2e.Classes.Consensus.Verification import SignedVerification
+from e2e.Classes.Consensus.VerificationPacket import VerificationPacket
+from e2e.Classes.Consensus.SpamFilter import SpamFilter
+
+from e2e.Vectors.Generation.PrototypeChain import PrototypeChain
+
+dataFilter: SpamFilter = SpamFilter(5)
+
+edPrivKey: ed25519.SigningKey = ed25519.SigningKey(b'\0' * 32)
+edPubKey: ed25519.VerifyingKey = edPrivKey.get_verifying_key()
+
+proto: PrototypeChain = PrototypeChain(40, keepUnlocked=True)
+proto.add(1)
+
+datas: List[Data] = [Data(bytes(32), edPubKey.to_bytes())]
+for d in range(2):
+  datas.append(Data(datas[0].hash, d.to_bytes(1, "little")))
+for data in datas:
+  data.sign(edPrivKey)
+  data.beat(dataFilter)
+
+verif: SignedVerification = SignedVerification(datas[1].hash)
+verif.sign(0, PrivateKey(0))
+proto.add(
+  packets=[
+    VerificationPacket(datas[0].hash, [0]),
+    VerificationPacket(datas[2].hash, [1])
+  ]
+)
+
+for _ in range(5):
+  proto.add()
+
+with open("e2e/Vectors/Consensus/Families/UnmentionedBeatMentioned.json", "w") as vectors:
+  vectors.write(json.dumps({
+    "blockchain": proto.toJSON(),
+    "datas": [data.toJSON() for data in datas],
+    "verification": verif.toSignedJSON()
+  }))
