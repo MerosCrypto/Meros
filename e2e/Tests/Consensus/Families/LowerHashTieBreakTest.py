1bb6aee7d1bf09f13aeaeddc004334b68c75cbf4
Luke Parker <lukeparker5132@gmail.com>
2020-10-27 05:56:22 -0500

    Add a lower hash tie break test.

---
+++ e2e/Tests/Consensus/Families/LowerHashTieBreakTest.py
@@ -0,0 +1,38 @@
+from typing import Dict, List, Any
+import json
+
+from e2e.Classes.Transactions.Data import Data
+
+from e2e.Meros.RPC import RPC
+from e2e.Meros.Liver import Liver
+
+from e2e.Tests.Errors import TestError
+
+def LowerHashTieBreakTest(
+  rpc: RPC
+) -> None:
+  vectors: Dict[str, Any]
+  with open("e2e/Vectors/Consensus/Families/LowerHashTieBreak.json", "r") as file:
+    vectors = json.loads(file.read())
+  datas: List[Data] = [Data.fromJSON(data) for data in vectors["datas"]]
+
+  def sendDatas() -> None:
+    for d in range(len(datas)):
+      if rpc.meros.liveTransaction(datas[d]) != rpc.meros.live.recv():
+        raise TestError("Meros didn't broadcast a Data.")
+
+  def verifyLowerHashWon() -> None:
+    data: Data = datas[1]
+    if int.from_bytes(data.hash, "little") > int.from_bytes(datas[2].hash, "little"):
+      data = datas[2]
+    if not rpc.call("consensus", "getStatus", [data.hash.hex()])["verified"]:
+      raise TestError("Meros didn't verify the tied Transaction with a lower hash.")
+
+  Liver(
+    rpc,
+    vectors["blockchain"],
+    callbacks={
+      40: sendDatas,
+      46: verifyLowerHashWon
+    }
+  ).live()
---
+++ e2e/Vectors/Generation/Consensus/Families/LowerHashTieBreak.py
@@ -0,0 +1,45 @@
+from typing import List
+import json
+
+import ed25519
+
+from e2e.Classes.Transactions.Data import Data
+
+from e2e.Classes.Consensus.VerificationPacket import VerificationPacket
+from e2e.Classes.Consensus.SpamFilter import SpamFilter
+
+from e2e.Vectors.Generation.PrototypeChain import PrototypeChain
+
+dataFilter: SpamFilter = SpamFilter(5)
+
+edPrivKey: ed25519.SigningKey = ed25519.SigningKey(b'\0' * 32)
+edPubKey: ed25519.VerifyingKey = edPrivKey.get_verifying_key()
+
+proto: PrototypeChain = PrototypeChain(20, keepUnlocked=True)
+for _ in range(20):
+  proto.add(1)
+
+datas: List[Data] = [Data(bytes(32), edPubKey.to_bytes())]
+for d in range(2):
+  datas.append(Data(datas[0].hash, d.to_bytes(1, "little")))
+for data in datas:
+  data.sign(edPrivKey)
+  data.beat(dataFilter)
+
+proto.add(
+  2,
+  packets=[
+    VerificationPacket(datas[0].hash, [0, 1]),
+    VerificationPacket(datas[1].hash, [0]),
+    VerificationPacket(datas[2].hash, [1])
+  ]
+)
+
+for _ in range(5):
+  proto.add(2)
+
+with open("e2e/Vectors/Consensus/Families/LowerHashTieBreak.json", "w") as vectors:
+  vectors.write(json.dumps({
+    "blockchain": proto.toJSON(),
+    "datas": [data.toJSON() for data in datas]
+  }))
