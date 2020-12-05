a472048bfe1734f2bb0212b47e7d629e9e76ca6d
Luke Parker <lukeparker5132@gmail.com>
2020-12-05 03:16:46 -0600

    Finish updating the Consensus test.

--- tests/Database/Consensus/ConsensusTest.nim
+++ tests/Database/Consensus/ConsensusTest.nim
@@ -126,6 +126,8 @@ suite "Consensus":
       packets: seq[VerificationPacket] = @[]
       #Elements to include in the next Block.
       elements: seq[BlockElement] = @[]
+      #Removals to include in the next Block.
+      removals: set[uint16] = {}
       #List of Transactions we didn't add every SignedVerification for.
       unsigned: seq[Hash[256]] = @[]
       #SignedVerification used to generate signatures.
@@ -139,7 +141,7 @@ suite "Consensus":
       var
         miner: MinerWallet
         mining: Block
-      if (rand(74) == 0) or (holders.len == 0):
+      if (rand(74) == 0) or (holders.len == merit.state.hasMR.card):
         miner = newMinerWallet()
         miner.nick = uint16(holders.len)
         holders.add(miner)
@@ -151,10 +153,13 @@ suite "Consensus":
           miner = miner,
           packets = packets,
           elements = elements,
+          removals = removals,
           aggregate = aggregate
         )
       else:
         var h: int = rand(high(holders))
+        while merit.state.hasMR.contains(uint16(h)):
+          h = rand(high(holders))
         miner = holders[h]
 
         mining = newBlankBlock(
@@ -165,6 +170,7 @@ suite "Consensus":
           miner = miner,
           packets = packets,
           elements = elements,
+          removals = removals,
           aggregate = aggregate
         )
 
@@ -172,12 +178,8 @@ suite "Consensus":
       for packet in mining.body.packets:
         consensus.add(merit.state, packet)
 
-      #Check who has their Merit removed.
-      var removed: Table[uint16, MeritRemoval] = initTable[uint16, MeritRemoval]()
-      for elem in mining.body.elements:
-        if elem of MeritRemoval:
-          consensus.flag(merit.blockchain, merit.state, cast[MeritRemoval](elem))
-          removed[elem.holder] = cast[MeritRemoval](elem)
+      #Remove Merit.
+      consensus.remove(merit.blockchain, merit.state, removals)
 
       #Add a Block to the Blockchain to generate a holder.
       merit.processBlock(mining)
@@ -193,10 +195,6 @@ suite "Consensus":
 
       #Archive the Epochs.
       consensus.archive(merit.state, mining.body.packets, mining.body.elements, epoch, changes)
-
-      #Have the Consensus handle every person who suffered a MeritRemoval.
-      for removee in removed.keys():
-        consensus.remove(removed[removee], rewardsState[removee, rewardsState.processedBlocks])
 
       #Add the elements.
       for elem in elements:
@@ -235,6 +233,10 @@ suite "Consensus":
 
       #Create a random amount of 'Transaction's.
       for _ in 0 ..< rand(2) + 1:
+        #Don't create any if there's no valid Merit Holders.
+        if holders.len == merit.state.hasMR.card:
+          break
+
         #Register the Transaction.
         var tx: Transaction = Transaction()
         tx.hash = newRandomHash()
@@ -246,7 +248,7 @@ suite "Consensus":
 
         #Grab random holders to sign the packet.
         for h in 0 ..< holders.len:
-          if rand(1) == 0:
+          if merit.state.hasMR.contains(uint16(h)) or (rand(1) == 0):
             continue
 
           packets[^1].holders.add(uint16(h))
@@ -265,6 +267,8 @@ suite "Consensus":
         #Make sure at least one holder signed the packet.
         if packets[^1].holders.len == 0:
           packets[^1].holders.add(uint16(rand(high(holders))))
+          while merit.state.hasMR.contains(packets[^1].holders[0]):
+            packets[^1].holders[0] = uint16(rand(high(holders)))
 
           sv = newSignedVerificationObj(packets[^1].hash)
           holders[int(packets[^1].holders[0])].sign(sv)
@@ -287,7 +291,7 @@ suite "Consensus":
 
           #Run against each Merit Holder.
           for h in 0 ..< holders.len:
-            if epoch[tx].contains(uint16(h)) or (rand(2) == 0):
+            if epoch[tx].contains(uint16(h)) or merit.state.hasMR.contains(uint16(h)) or (rand(2) == 0):
               continue
 
             #Add the holder.
@@ -309,40 +313,31 @@ suite "Consensus":
             packets.del(high(packets))
 
       #Add Difficulties.
-      var
-        holder: int = rand(holders.len - 1)
-        sendDiff: SignedSendDifficulty
-        dataDiff: SignedDataDifficulty
-      sendDiff = newSignedSendDifficultyObj(consensus.getArchivedNonce(uint16(holder)) + 1, uint32(rand(high(int32))))
-      sendDiff.holder = uint16(holder)
-      elements.add(sendDiff)
+      if holders.len != merit.state.hasMR.card:
+        var
+          holder: int = rand(holders.len - 1)
+          sendDiff: SignedSendDifficulty
+          dataDiff: SignedDataDifficulty
+        while merit.state.hasMR.contains(uint16(holder)):
+          holder = rand(holders.len - 1)
+        sendDiff = newSignedSendDifficultyObj(consensus.getArchivedNonce(uint16(holder)) + 1, uint32(rand(high(int32))))
+        sendDiff.holder = uint16(holder)
+        elements.add(sendDiff)
 
-      holder = rand(holders.len - 1)
-      dataDiff = newSignedDataDifficultyObj(consensus.getArchivedNonce(uint16(holder)) + 1, uint32(rand(high(int32))))
-      dataDiff.holder = uint16(holder)
-      elements.add(dataDiff)
+        holder = rand(holders.len - 1)
+        while merit.state.hasMR.contains(uint16(holder)):
+          holder = rand(holders.len - 1)
+        dataDiff = newSignedDataDifficultyObj(consensus.getArchivedNonce(uint16(holder)) + 1, uint32(rand(high(int32))))
+        dataDiff.holder = uint16(holder)
+        elements.add(dataDiff)
 
+      removals = {}
       if rand(125) == 0:
         #Add a Merit Removal.
-        holder = rand(holders.len - 1)
+        var holder: int = rand(holders.len - 1)
         while merit.state[uint16(holder), merit.state.processedBlocks] == 0:
           holder = rand(holders.len - 1)
-
-        var
-          e1: SendDifficulty
-          e2: DataDifficulty
-        e1 = newSendDifficultyObj(0, uint32(rand(high(int32))))
-        e1.holder = uint16(holder)
-        e2 = newDataDifficultyObj(0, uint32(rand(high(int32))))
-        e2.holder = uint16(holder)
-
-        elements.add(newMeritRemoval(
-          uint16(holder),
-          false,
-          e1,
-          e2,
-          merit.state.holders
-        ))
+        removals.incl(uint16(holder))
 
       #Mine the packets.
       mineBlock()
