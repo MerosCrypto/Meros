34470df9af6dfe57ba463dbd858f44c9e1e13b8c
Luke Parker <lukeparker5132@gmail.com>
2020-12-20 07:46:11 -0600

    Clean up the Address Test.
    I missed a lint warning about too many statements. I could've just 
    disabled it, yet the repeated test block was pretty poor. Hence the 
    revamp.

--- e2e/Tests/RPC/AddressTest.py
+++ e2e/Tests/RPC/AddressTest.py
@@ -12,26 +12,31 @@ def encodeAddress(
 ) -> str:
   return bech32_encode("mr", convertbits(data, 8, 5))
 
+def test(
+  rpc: RPC,
+  address: Union[bytes, str],
+  invalid: bool,
+  msg: str
+) -> None:
+  try:
+    if isinstance(address, bytes):
+      address = encodeAddress(address)
+    rpc.call("personal", "send", [address, "1"])
+    #Raise a TestError with a different code than expected to ensure the below check is run and fails.
+    raise TestError("0 ")
+  except TestError as e:
+    if int(e.message.split(" ")[0]) != (-3 if invalid else 1):
+      raise TestError(msg)
+
 def AddressTest(
   rpc: RPC
 ) -> None:
   #Sanity test.
-  try:
-    rpc.call("personal", "send", [encodeAddress(bytes(33)), "1"])
-    #Raise a TestError with a different code than expected to ensure the below check is run and fails.
-    raise TestError("0 ")
-  except TestError as e:
-    if int(e.message.split(" ")[0]) != 1:
-      raise TestError("Meros didn't use the NotEnoughMeros error when trying to send while having 0 Meros.")
+  test(rpc, bytes(33), False, "Meros didn't use the NotEnoughMeros error when trying to send while having 0 Meros.")
 
   #Test a variety of valid addresses.
   for _ in range(50):
-    try:
-      rpc.call("personal", "send", [encodeAddress(bytes([0]) + os.urandom(32)), "1"])
-      raise TestError("0 ")
-    except TestError as e:
-      if int(e.message.split(" ")[0]) != 1:
-        raise TestError("Meros didn't use the NotEnoughMeros error when trying to send while having 0 Meros.")
+    test(rpc, bytes([0]) + os.urandom(32), False, "Meros didn't use the NotEnoughMeros error when trying to send while having 0 Meros.")
 
   #Invalid checksum.
   invalidChecksum: str = encodeAddress(bytes(33))
@@ -39,40 +44,21 @@ def AddressTest(
     invalidChecksum = invalidChecksum[:-1] + 'q'
   else:
     invalidChecksum = invalidChecksum[:-1] + 't'
-  try:
-    rpc.call("personal", "send", [invalidChecksum, "1"])
-    raise TestError("0 ")
-  except TestError as e:
-    if int(e.message.split(" ")[0]) != -3:
-      raise TestError("Meros accepted an address with an invalid checksum")
+  test(rpc, invalidChecksum, True, "Meros accepted an address with an invalid checksum")
 
   #Invalid version byte. 255 was used as it's expected version bytes will become VarInts if ever needed.
   #That said, even 127 is high enough we're likely to never come close.
-  try:
-    rpc.call("personal", "send", [encodeAddress(bytes([255]) + bytes(32)), "1"])
-    raise TestError("0 ")
-  except TestError as e:
-    if int(e.message.split(" ")[0]) != -3:
-      raise TestError("Meros accepted an address with an invalid version byte.")
+  test(rpc, bytes([255]) + bytes(32), True, "Meros accepted an address with an invalid version byte.")
 
   #Invalid length.
-  try:
-    rpc.call("personal", "send", [encodeAddress(bytes(34)), "1"])
-    raise TestError("0 ")
-  except TestError as e:
-    if int(e.message.split(" ")[0]) != -3:
-      raise TestError("Meros accepted an address with an invalid length.")
+  test(rpc, bytes(32), True, "Meros accepted an address with an invalid length.")
+  test(rpc, bytes(34), True, "Meros accepted an address with an invalid length.")
 
   #Create a random address for us to mutate.
   randomKey: bytes = os.urandom(32)
   unchanged: str = encodeAddress(bytes([0]) + randomKey)
   #Sanity check against it.
-  try:
-    rpc.call("personal", "send", [unchanged, "1"])
-    raise TestError("0 ")
-  except TestError as e:
-    if int(e.message.split(" ")[0]) != 1:
-      raise TestError("Meros didn't use the NotEnoughMeros error when trying to send while having 0 Meros.")
+  test(rpc, unchanged, False, "Meros didn't use the NotEnoughMeros error when trying to send while having 0 Meros.")
 
   #Mutate it as described in https://github.com/sipa/bech32/issues/51#issuecomment-496797984.
   #Since we can insert any amount of 'q's, run this ten times.
@@ -82,18 +68,13 @@ def AddressTest(
   for i in range(10):
     mutated: str = unchanged[:-1] + CHARSET[CHARSET.find(unchanged[-1]) ^ 1]
     #Just i would be 0 on the first run, which would be a NOP.
+    #Therefore, a valid and unmodified address.
     mutated += "q" * (i + 1)
     mutated = mutated[:-1] + CHARSET[CHARSET.find(mutated[-1]) ^ 1]
 
-    #Ensure it decodes properly with the library itself.
     #Sanity check that our mutation worked.
     decoded: Union[Tuple[None, None], Tuple[str, List[int]]] = bech32_decode(mutated)
     if decoded is Tuple[None, None]:
       raise TestError("Mutation stopped the checksum from passing.")
 
-    try:
-      rpc.call("personal", "send", [mutated, "1"])
-      raise TestError("0 ")
-    except TestError as e:
-      if int(e.message.split(" ")[0]) != -3:
-        raise TestError("Meros accepted an address which had been mutated yet still passed the checksum.")
+    test(rpc, mutated, True, "Meros accepted an address which had been mutated yet still passed the checksum.")
